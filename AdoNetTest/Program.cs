5a500a3f10b51d186ab84b5e276418168337c094
Max Rosegger <rosegg130192@sr.htlweiz.at>
2018-11-28 08:36:17 +0100

    Connects to DB

--- AdoNetTest/Program.cs
+++ AdoNetTest/Program.cs
@@ -18,7 +18,6 @@ namespace AdoNetTest
             string conString = "Server=127.0.0.1;Port=5432;Database=testDB;User Id=testuser;Password = 1234; ";
             NpgsqlConnection newConnection = new NpgsqlConnection(conString);
             newConnection.Open();
-
             Console.ReadKey();
             newConnection.Close();
            
