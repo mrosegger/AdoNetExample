8635465106c1a8f6fe9a2f1acd2a00d9c5196a6e
Max Rosegger <rosegg130192@sr.htlweiz.at>
2018-11-28 08:35:19 +0100

    Add project files.

---
+++ AdoNetTest/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.Odbc;
+using Npgsql;
+
+
+namespace AdoNetTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string conString = "Server=127.0.0.1;Port=5432;Database=testDB;User Id=testuser;Password = 1234; ";
+            NpgsqlConnection newConnection = new NpgsqlConnection(conString);
+            newConnection.Open();
+
+            Console.ReadKey();
+            newConnection.Close();
+           
+        }
+    }
+}
